736de3b4996c320f8c19c8b8ac5580d1050e8ca1
manon <163267219+220802cm@users.noreply.github.com>
2024-04-26 20:06:29 +0200

    task3

---
+++ task3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    long int numb1 = 6466865;
+    long int numb2 = 5440044;
+    long int mult = numb1 * numb2;
+    printf("The multiplication betwen %ld and %ld is %ld\n ", numb1, numb2, mult);
+}
